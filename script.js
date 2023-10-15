c4950204770e2edaf81479e552b8760534a834a3
Vikas <81951052+vkas9@users.noreply.github.com>
2023-10-15 11:26:49 +0000

    start

---
+++ script.js
@@ -0,0 +1,26 @@
+Shery.imageEffect(".back",{style:2});
+Shery.makeMagnet(".magnet-target" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+var h1s=document.querySelectorAll("h1");
+var index=0;
+document.querySelector(".main").addEventListener("click",()=>{
+    gsap.to(h1s[index],{
+        top:"-=100%",
+        ease:Expo.easeInOut,
+        duration:1,
+        onComplete:function(){
+            gsap.set(this.targets[0],{top:"100%"});
+        },
+    });
+    
+    index===h1s.length-1?(index=0):index++;
+    gsap.to(h1s[index],{
+        top:"-=100%",
+        ease:Expo.easeInOut,
+        duration:1,
+    });
+    
+})
